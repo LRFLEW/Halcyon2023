bdf3334daf9afc38c96c6e699a643539badbf699
LRFLEW <LRFLEW@aol.com>
2023-07-30 20:09:22 -0500

    Add idle animation and sound effects

--- Button.gd
+++ Button.gd
@@ -2,9 +2,17 @@ extends Area2D
 
 signal button_state(state : bool)
 
+var prev_state := false
+
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var press_sound : AudioStreamPlayer = $PressSound
+@onready var depress_sound : AudioStreamPlayer = $DepressSound
 
 func on_body(body : Node2D):
 	var press := has_overlapping_bodies()
-	sprite.play("Pressed" if press else "Unpressed")
-	button_state.emit(press)
+	if press != prev_state:
+		prev_state = press
+		sprite.play("Pressed" if press else "Unpressed")
+		if press: press_sound.play()
+		else: depress_sound.play()
+		button_state.emit(press)
--- Player.gd
+++ Player.gd
@@ -2,15 +2,27 @@ extends CharacterBody2D
 
 const SPEED := 350.0
 const JUMP_VELOCITY := -500.0
+const WORLD_END := 800.0
 
+@export var do_respawn := true
+@export var respawn_timer := 0.6
 @export var hshift := 4.0
+@export var footstep_time := 0.33
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var timer := 0.0
+var ft_timer := footstep_time
 
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var footsteps : AudioStreamPlayer = $Footsteps
 
 func _physics_process(delta):
+	if do_respawn and position.y > WORLD_END:
+		timer += delta
+		if timer >= respawn_timer:
+			get_tree().reload_current_scene()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -23,6 +35,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("Left", "Right")
 	velocity.x = direction * SPEED
 	
+	ft_timer += delta
 	if direction < 0.0:
 		sprite.flip_h = true
 		sprite.position.x = -hshift
@@ -31,6 +44,9 @@ func _physics_process(delta):
 		sprite.position.x = 0.0
 	if abs(direction) > 0.0:
 		sprite.play("Walk")
+		if ft_timer >= footstep_time and is_on_floor():
+			ft_timer = 0.0
+			footsteps.play()
 	else:
 		sprite.play("Idle")
 	
