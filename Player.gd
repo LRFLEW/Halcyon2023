0da5883244e156f32ba4a393641a71442fa22402
LRFLEW <LRFLEW@aol.com>
2023-07-30 21:24:48 -0500

    Add jump animations

--- Player.gd
+++ Player.gd
@@ -35,20 +35,34 @@ func _physics_process(delta):
 	var direction = Input.get_axis("Left", "Right")
 	velocity.x = direction * SPEED
 	
-	ft_timer += delta
 	if direction < 0.0:
 		sprite.flip_h = true
 		sprite.position.x = -hshift
 	elif direction > 0.0:
 		sprite.flip_h = false
 		sprite.position.x = 0.0
-	if abs(direction) > 0.0:
-		sprite.play("Walk")
-		if ft_timer >= footstep_time and is_on_floor():
-			ft_timer = 0.0
-			footsteps.play()
+	
+	ft_timer += delta
+	if is_on_floor():
+		if abs(direction) > 0.0:
+			if sprite.animation != "Walk":
+				sprite.play("Walk")
+			if ft_timer >= footstep_time and is_on_floor():
+				ft_timer = 0.0
+				footsteps.play()
+		else:
+			if sprite.animation != "JumpLand" and sprite.animation != "Idle":
+				if sprite.animation.begins_with("Jump"):
+					sprite.play("JumpLand")
+				else:
+					sprite.play("Idle")
 	else:
-		sprite.play("Idle")
+		if velocity.y < 0.0:
+			if sprite.animation != "JumpRise":
+				sprite.play("JumpRise")
+		else:
+			if sprite.animation != "JumpTransition" and sprite.animation != "JumpFall":
+				sprite.play("JumpTransition")
 	
 	var has_col := move_and_slide()
 	if has_col:
@@ -56,3 +70,10 @@ func _physics_process(delta):
 			var col := get_slide_collision(i)
 			if col.get_collider() is RigidBody2D:
 				var ncol = col.get_collider().move_and_collide(Vector2(col.get_remainder().x, 0.0))
+
+func animation_next():
+	match sprite.animation:
+		"JumpTransition":
+			sprite.play("JumpFall")
+		"JumpLand":
+			sprite.play("Idle")
